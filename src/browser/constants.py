323bd50d5aca4b8936b64641a12658d4479d93d6
yie1d <myie1d@163.com>
2025-05-12 16:08:44 +0800

    refactor: refactor project

---
+++ src/browser/constants.py
@@ -0,0 +1,17 @@
+from enum import IntEnum, auto
+
+
+class State(IntEnum):
+    """
+    已初始化
+    已启动
+    已停止
+    """
+    INITIALIZED: int = auto()
+    STARTED: int = auto()
+    STOPPED: int = auto()
+
+
+class BrowserType(IntEnum):
+    CHROME: int = auto()
+    EDGE: int = auto()
