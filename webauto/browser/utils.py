9293c7f3b2412d31b477a5c28e8c583641385da9
yie1d <myie1d@163.com>
2025-07-11 14:09:04 +0800

    feat(browser): add image format detection and base64 decoding utilities
    - Add `decode_base64_to_bytes` function to decode base64-encoded images
    - Implement `get_path_ext` function to extract file extension from path- Create `get_img_format` function to validate and return image format
    - Update import statements for new functionalities

--- webauto/browser/utils.py
+++ webauto/browser/utils.py
@@ -1,6 +1,37 @@
+import base64
+from pathlib import Path
+from typing import Literal
+
 from cdpkit.connection import CDPSessionExecutor
+from cdpkit.logger import logger
 from cdpkit.protocol import Runtime
-from src.logger import logger
+
+
+def decode_base64_to_bytes(image: str) -> bytes:
+    return base64.b64decode(image.encode('utf-8'))
+
+
+def get_path_ext(path: Path | str | None) -> str | None:
+    if path is None:
+        return None
+
+    if isinstance(path, str):
+        path = Path(path)
+    elif isinstance(path, Path):
+        pass
+    else:
+        raise TypeError(f'Invalid path type {type(path)}, only support str and Path type')
+
+    return path.suffix.lower().removeprefix('.')
+
+
+def get_img_format(path: Path | str | None) -> Literal['jpeg', 'png', 'webp'] | None:
+    ext = get_path_ext(path)
+
+    if ext in ['jpeg', 'png', 'webp']:
+        return ext
+    else:
+        raise TypeError(f'Invalid image format: {ext}, only jpeg, png, webp are supported')
 
 
 class RuntimeParser:
